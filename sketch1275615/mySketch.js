599e9ca70bc78b94497323d7cf8274bd1466f995
HoumingS <hs3899@nyu.edu>
2021-10-25 15:14:09 -0400

    this is my first push
    in class learning how to use github

---
+++ sketch1275615/mySketch.js
@@ -0,0 +1,65 @@
+size(800,800);
+background(250,190,148);
+
+noStroke();
+
+fill(250,148,209,150);
+rect(200,200,400,400);
+ellipse(150,190,200,300);
+ellipse(500,170,430,260);
+ellipse(650,700,250,250);
+rect(100,500,150,750);
+
+fill(245,175,155,160);
+rect(400,150,500,650);
+ellipse(185,550,300,400);
+
+fill(178,111,227,100);
+rect(300,300,200,200);
+
+fill(135,148,255,100);
+rect(350,350,100,100);
+
+beginShape();
+vertex(400, 300);
+vertex(500, 400);
+vertex(400, 500);
+vertex(300, 400);
+endShape(CLOSE);
+
+
+beginShape(TRIANGLE_FAN);
+vertex(326, 340);
+vertex(245, 260); 
+vertex(368, 456); 
+vertex(245, 145); 
+vertex(88, 400); 
+vertex(345, 123); 
+endShape();
+
+beginShape(TRIANGLE_FAN);
+vertex(452, 567);
+vertex(673, 723); 
+vertex(530, 540); 
+vertex(433, 395); 
+vertex(370, 590); 
+vertex(452, 567); 
+endShape();
+
+beginShape(TRIANGLE_FAN);
+vertex(234, 640);
+vertex(400, 430); 
+vertex(128, 630); 
+vertex(90, 550); 
+vertex(150, 800); 
+vertex(230, 640); 
+endShape();
+
+beginShape(TRIANGLE_FAN);
+vertex(400, 400);
+vertex(370, 80); 
+vertex(710, 199); 
+vertex(638, 61); 
+vertex(547, 179); 
+vertex(400, 400); 
+endShape();
